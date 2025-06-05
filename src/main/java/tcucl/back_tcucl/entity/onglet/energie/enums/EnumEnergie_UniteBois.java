6ddb078ce188969bac64fcf015566665ad30c73a
Chloe LELONG <chloe.lelong@student.junia.com>
2025-06-05 15:56:34 +0200

    fix result

--- src/main/java/tcucl/back_tcucl/entity/onglet/energie/enums/EnumEnergie_UniteBois.java
+++ src/main/java/tcucl/back_tcucl/entity/onglet/energie/enums/EnumEnergie_UniteBois.java
@@ -1,17 +1,23 @@
 package tcucl.back_tcucl.entity.onglet.energie.enums;
 
 public enum EnumEnergie_UniteBois {
-    TONNE(0),
-    MWH_PCS(1);
+    TONNE(0, "kgCO2e/tonnes"),
+    MWH_PCS(1, "kgCO2e/MWh PCS");
 
     private final int code;
+    private final String libelle;
 
-    EnumEnergie_UniteBois(int code) {
+    EnumEnergie_UniteBois(int code, String libelle) {
         this.code = code;
+        this.libelle = libelle;
     }
 
     public Integer getCode() {
         return code;
+    }
+
+    public String getLibelle() {
+        return libelle;
     }
 
     public static EnumEnergie_UniteBois fromCode(int code) {
--- src/main/java/tcucl/back_tcucl/entity/onglet/energie/enums/EnumEnergie_UniteGaz.java
+++ src/main/java/tcucl/back_tcucl/entity/onglet/energie/enums/EnumEnergie_UniteGaz.java
@@ -1,17 +1,23 @@
 package tcucl.back_tcucl.entity.onglet.energie.enums;
 
 public enum EnumEnergie_UniteGaz {
-    MWH_PCS(0),
-    M3(1);
+    MWH_PCS(0, "kgCO2e/MWh PCS"),
+    M3(1, "kgCO2e/m3");
 
     private final int code;
+    private final String libelle;
 
-    EnumEnergie_UniteGaz(int code) {
+    EnumEnergie_UniteGaz(int code, String libelle) {
         this.code = code;
+        this.libelle = libelle;
     }
 
     public Integer getCode() {
         return code;
+    }
+
+    public String getLibelle() {
+        return libelle;
     }
 
     public static EnumEnergie_UniteGaz fromCode(int code) {
