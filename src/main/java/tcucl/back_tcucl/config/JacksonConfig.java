38c386071058b9fa3c345611230697d9516e5765
Pierre Olivier <pierre-olivier.malphettes@student.junia.com>
2025-07-09 14:34:16 +0200

    suppression config inutile Jackson

--- src/main/java/tcucl/back_tcucl/config/JacksonConfig.java
+++ src/main/java/tcucl/back_tcucl/config/JacksonConfig.java
@@ -1,8 +1,5 @@
 package tcucl.back_tcucl.config;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
